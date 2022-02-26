eac5b721ac5c14001eb73939fb17adb6b1739d53
canhuihu <xianshixiaohuo123@163.com>
2022-02-26 10:56:19 +0800

    调整了初始目录，引入了Element组件库

--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// 引入 Element
+import ElementUI from 'element-ui'
+// 引入Element 的主题文件
+import 'element-ui/lib/theme-chalk/index.css'
+// 将 Element 注册为 Vue 插件
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
