50f010c90d9b1496de38f3e56ac3316e5bd26c5b
canhuihu <xianshixiaohuo123@163.com>
2022-02-27 16:17:12 +0800

    全局样式，路由基础功能以及功能优化

---
+++ src/views/error-page/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="404">404 Not Found.</div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound'
+}
+
+</script>
+<style lang="scss" scoped>
+</style>
---
+++ src/views/menu/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>菜单</div>
+</template>
+
+<script>
+export default {
+  name: 'Menu'
+}
+
+</script>
+<style lang="scss" scoped>
+</style>
